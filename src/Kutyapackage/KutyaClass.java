26300797fce2b4c6ade25ed25de533fee9244bb7
Tamás Varga <vargatamas88@gmail.com>
2021-01-23 20:57:33 +0100

    Kutyafeladat

---
+++ src/Kutyapackage/KutyaClass.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Kutyapackage;
+
+/**
+ *
+ * @author Tomi
+ */
+public class KutyaClass {
+    
+    private String fajta;
+    private String nem;
+    private  int eletkor;
+    private String nev;
+
+    public KutyaClass(String sor) {
+        String[] d = sor.split(";");
+        this.fajta = d[0];
+        //this.nem = d[1];
+        //this.eletkor = Integer.parseInt(d[2]);
+        this.nev = d[3];
+    }
+
+    public String getFajta() {
+        return fajta;
+    }
+
+    public String getNem() {
+        return nem;
+    }
+
+    public int getEletkor() {
+        return eletkor;
+    }
+
+
+    public String getNev() {
+        return nev;
+    }
+    
+    
+    
+    
+}
